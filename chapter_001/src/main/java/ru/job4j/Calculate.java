85b26c4b2395ecf53f677421960fbd21e8ea5bdf
ilya-moskovtsev <ilya.moskovtsev@gmail.com>
2017-03-05 16:18:33 +0300

    task 9712 1.3. JavaDoc. Code convention

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,15 @@
 package ru.job4j;
 
+/**
+* Class Calculate решение задачи части 001 урок 1.
+* @author imoskovtsev
+* @since 05.03.2017
+*/
 public class Calculate{
+	/**
+	* Вывод строки в консоль.
+	* @param args - args.
+	*/
 	public static void main(String[] args){
 		System.out.println("Hello World!");
 	}

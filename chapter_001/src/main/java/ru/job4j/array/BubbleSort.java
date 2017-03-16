b2756e47090aed85590f870a975341f5fe2e1c05
ilya-moskovtsev <ilya.moskovtsev@gmail.com>
2017-03-16 21:19:36 +0300

    5.1. Создать программу для сортировки массива методом перестановки. [#10455]

---
+++ chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
@@ -0,0 +1,23 @@
+package ru.job4j.array;
+/**
+* 5.1. Создать программу для сортировки массива методом перестановки.
+*/
+public class BubbleSort {
+	/**
+	* @param array - массив, который необходимо перевернуть.
+	* @return int[] - перевернутый массив.
+	* Метод должен сортировать массив целых чисел, используя алгоритм сортировки пузырьком.
+	*/
+	public int[] sort(int[] array) {
+		for (int i = array.length - 1; i >= 0; i--) {
+			for (int j = 0; j < i; j++) {
+				if (array[j] > array[j + 1]) {
+					int temp = array[j];
+					array[j] = array[j + 1];
+					array[j + 1] = temp;
+				}
+			}
+		}
+		return array;
+	}
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/BubbleSortTest.java
@@ -0,0 +1,22 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+/**
+* Проверка 5.1. Создать программу для сортировки массива методом перестановки.
+*/
+public class BubbleSortTest {
+	/**
+	* Проверка сортировки массива.
+	* Массив {4, 1, 6, 2} после сортировки должен получиться {1, 2, 4, 6}
+	*/
+	@Test
+	public void whenEvenNumberOfElementsThenReverse() {
+		BubbleSort bubbleSort = new BubbleSort();
+		int[] resultArray = bubbleSort.sort(new int[] {4, 1, 6, 2});
+		int[] expectedArray = new int[] {1, 2, 4, 6};
+		assertThat(resultArray, is(expectedArray));
+	}
+}

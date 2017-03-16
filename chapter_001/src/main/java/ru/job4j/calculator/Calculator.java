a9d428c2278c03b454a7525cd27912fa4ddd7264
ilya-moskovtsev <ilya.moskovtsev@gmail.com>
2017-03-16 23:24:56 +0300

    1.3. JavaDoc. Code convention [#9712]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -10,43 +10,43 @@ public class Calculator {
     private double result;
 
     /**
-     * adds second to first
-     * @param first first
-     * @param second
+     * adds second to first.
+     * @param first - first value
+     * @param second - second value
      */
     public void add(double first, double second) {
         this.result = first + second;
     }
 
     /**
-     * subtracts second from first
-     * @param first first
-     * @param second
+     * subtracts second from first.
+     * @param first - first value
+     * @param second - second value
      */
     public void subtract(double first, double second) {
         this.result = first - second;
     }
 
     /**
-     * devides first by second
-     * @param first first
-     * @param second
+     * devides first by second.
+     * @param first - first value
+     * @param second - second value
      */
     public void devide(double first, double second) {
         this.result = first / second;
     }
 
     /**
-     * multiplies first by second
-     * @param first first
-     * @param second
+     * multiplies first by second.
+     * @param first - first value
+     * @param second - second value
      */
     public void multiply(double first, double second) {
         this.result = first * second;
     }
 
     /**
-     * Returns calculation result
+     * Returns calculation result.
      * @return result
      */
     public double getResult() {

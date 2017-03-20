156846f23e61f68012d38b00004ca3739c664c2a
ilya-moskovtsev <ilya.moskovtsev@gmail.com>
2017-03-20 20:39:30 +0300

    5.2. Создание программы поворота квадратного массива. [#10616]

--- chapter_001/src/main/java/ru/job4j/array/RotateArray.java
+++ chapter_001/src/main/java/ru/job4j/array/RotateArray.java
@@ -8,23 +8,22 @@ public class RotateArray {
 	* Метод должен повернуть двумерный массив по часовой стрелке.
 	* Задачу можно решить с использованием дополнительного массива, так и без него.
 	* @param array - исходный массив.
-	* @return int[][] - массив повернутый по часовой стрелке.
+	* @return int[][] - массив повернутый по часовой стрелке на 90 градусов.
 	*/
 	public int[][] rotate(int[][] array) {
-		if (array.length == 2) {
-			int temp = array[0][0];
-			array[0][0] = array[1][0];
-			array[1][0] = array[1][1];
-			array[1][1] = array[0][1];
-			array[0][1] = temp;
-		} else if (array.length > 2) {
-			int temp = 0;
-			for (int i = 0; i < array.length - 1; i++) {
-				temp = array[0][i];
-				array[0][i] = array[array.length - 1 - i][0];
-				array[array.length - 1 - i][0] = array[array.length - 1][array.length - 1 - i];
-				array[array.length - 1][array.length - 1 - i] = array[i][array.length - 1];
-				array[i][array.length - 1] = temp;
+		int stop1 = array.length / 2;
+		int stop2 = array.length / 2;
+		if (array.length % 2 != 0) {
+			stop2++;
+		}
+		int temp = 0;
+		for (int i = 0; i < stop1; i++) {
+			for (int j = 0; j < stop2; j++) {
+				temp = array[j][i];
+				array[j][i] = array[array.length - 1 - i][j];
+				array[array.length - 1 - i][j] = array[array.length - 1 - j][array.length - 1 - i];
+				array[array.length - 1 - j][array.length - 1 - i] = array[i][array.length - 1 - j];
+				array[i][array.length - 1 - j] = temp;
 			}
 		}
 		return array;
--- chapter_001/src/test/java/ru/job4j/array/RotateArrayTest.java
+++ chapter_001/src/test/java/ru/job4j/array/RotateArrayTest.java
@@ -39,4 +39,14 @@ public class RotateArrayTest {
 		int[][] expectedArray = new int[][] {{13, 9, 5, 1}, {14, 10, 6, 2}, {15, 11, 7, 3}, {16, 12, 8, 4}};
 		assertThat(resultArray, is(expectedArray));
 	}
+	/**
+	* Проверка поворота двумерного массив 5 X 5 по часовой стрелке.
+	*/
+	@Test
+	public void whenRotate5X5ArrayThenResult() {
+		RotateArray rotateArray = new RotateArray();
+		int[][] resultArray = rotateArray.rotate(new int[][] {{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {11, 12, 13, 14, 15}, {16, 17, 18, 19, 20}, {21, 22, 23, 24, 25}});
+		int[][] expectedArray = new int[][] {{21, 16, 11, 6, 1}, {22, 17, 12, 7, 2}, {23, 18, 13, 8, 3}, {24, 19, 14, 9, 4}, {25, 20, 15, 10, 5}};
+		assertThat(resultArray, is(expectedArray));
+	}
 }

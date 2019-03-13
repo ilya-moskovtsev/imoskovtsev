134aa555df4ce9512d49f9a3c9efd96d4b4caf49
ilya-moskovtsev <ilya.moskovtsev@gmail.com>
2019-03-13 21:32:13 +0300

    Generic Storage. [#111119]

--- junior/chapter_001_collections_pro/src/main/java/ru/job4j/generic/RoleStorage.java
+++ junior/chapter_001_collections_pro/src/main/java/ru/job4j/generic/RoleStorage.java
@@ -1,6 +1,6 @@
 package ru.job4j.generic;
 
-public class RoleStorage extends AbstractStorage<Role> implements Storage<Role> {
+public class RoleStorage extends AbstractStorage<Role> {
     public RoleStorage(int capacity) {
         super(capacity);
     }
--- junior/chapter_001_collections_pro/src/main/java/ru/job4j/generic/UserStorage.java
+++ junior/chapter_001_collections_pro/src/main/java/ru/job4j/generic/UserStorage.java
@@ -1,6 +1,6 @@
 package ru.job4j.generic;
 
-public class UserStorage extends AbstractStorage<User> implements Storage<User> {
+public class UserStorage extends AbstractStorage<User> {
     public UserStorage(int capacity) {
         super(capacity);
     }

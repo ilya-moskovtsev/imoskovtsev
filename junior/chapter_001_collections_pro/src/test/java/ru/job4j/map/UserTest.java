b6e41d564e8432083ba094e482fd0d42a0db8ce4
ilya-moskovtsev <ilya.moskovtsev@gmail.com>
2019-03-20 17:12:03 +0300

    equals is overridden [#111128]

--- junior/chapter_001_collections_pro/src/test/java/ru/job4j/map/UserTest.java
+++ junior/chapter_001_collections_pro/src/test/java/ru/job4j/map/UserTest.java
@@ -8,9 +8,10 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertTrue;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.junit.Assert.assertThat;
 
 public class UserTest {
 
@@ -41,8 +42,8 @@ public class UserTest {
     @Test
     public void equalsAndHashCodeAreNotOverridden() {
         // two users have same fields
-        User first = new User(name, children, calendar);
-        User second = new User(name, children, calendar);
+        var first = new User(name, children, calendar);
+        var second = new User(name, children, calendar);
 
         Map<User, Object> map = new HashMap<>();
 
@@ -51,8 +52,9 @@ public class UserTest {
 
         System.out.println(map);
 
-        assertTrue("hashCode is different", first.hashCode() != second.hashCode());
-        assertNotEquals("users not equal", first, second);
+        assertThat("hashCode is different", first.hashCode(), not(second.hashCode()));
+        assertThat("users not equal", first, not(second));
+        assertThat("Map has both users", map.size(), is(2));
     }
 
     /**
@@ -78,8 +80,8 @@ public class UserTest {
     @Test
     public void hashCodeIsOverridden() {
         // two users have same fields
-        User2 first = new User2(name, children, calendar);
-        User2 second = new User2(name, children, calendar);
+        var first = new User2(name, children, calendar);
+        var second = new User2(name, children, calendar);
 
         Map<User2, Object> map = new HashMap<>();
 
@@ -88,7 +90,66 @@ public class UserTest {
 
         System.out.println(map);
 
-        assertEquals("hashCode is the same", first.hashCode(), second.hashCode());
-        assertNotEquals("users not equal", first, second);
+        assertThat("hashCode is the same", first.hashCode(), equalTo(second.hashCode()));
+        assertThat("users not equal", first, not(second));
+        assertThat("Map has both users", map.size(), is(2));
+    }
+
+    /**
+     * Equals is overridden.
+     */
+    public static class User3 {
+        public String name;
+        public int children;
+        public Calendar birthday;
+
+        public User3(String name, int children, Calendar birthday) {
+            this.name = name;
+            this.children = children;
+            this.birthday = birthday;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            User3 user3 = (User3) o;
+            return children == user3.children
+                    && Objects.equals(name, user3.name)
+                    && Objects.equals(birthday, user3.birthday);
+        }
+
+        /**
+         * --- maven-checkstyle-plugin:2.17:check (validate) @ chapter_001_collections_pro ---
+         * error: Definition of 'equals()' without corresponding definition of 'hashCode()'
+         *
+         * @return result of default hashCode method
+         */
+        @Override
+        public int hashCode() {
+            return super.hashCode();
+        }
+    }
+
+    @Test
+    public void equalsIsOverridden() {
+        // two users have same fields
+        var first = new User3(name, children, calendar);
+        var second = new User3(name, children, calendar);
+
+        Map<User3, Object> map = new HashMap<>();
+
+        map.put(first, new Object());
+        map.put(second, new Object());
+
+        System.out.println(map);
+
+        assertThat("hashCode is different", first.hashCode(), not(second.hashCode()));
+        assertThat("users are equal", first, equalTo(second));
+        assertThat("Map has both users", map.size(), is(2));
     }
 }

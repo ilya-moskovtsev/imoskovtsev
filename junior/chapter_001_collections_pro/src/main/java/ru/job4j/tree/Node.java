c78010a6d6b013c67f20a0d81b4ba03b53628606
ilya-moskovtsev <ilya.moskovtsev@gmail.com>
2019-04-01 14:20:28 +0300

    simple tree [#111138]

---
+++ junior/chapter_001_collections_pro/src/main/java/ru/job4j/tree/Node.java
@@ -0,0 +1,29 @@
+package ru.job4j.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Node<E extends Comparable<E>> {
+    private final List<Node<E>> children = new ArrayList<>();
+    private final E value;
+
+    public Node(E value) {
+        this.value = value;
+    }
+
+    public E getValue() {
+        return value;
+    }
+
+    public void add(Node<E> child) {
+        this.children.add(child);
+    }
+
+    public List<Node<E>> leaves() {
+        return this.children;
+    }
+
+    public boolean eqValue(E that) {
+        return this.value.compareTo(that) == 0;
+    }
+}
---
+++ junior/chapter_001_collections_pro/src/main/java/ru/job4j/tree/SimpleTreeClass.java
@@ -0,0 +1,81 @@
+package ru.job4j.tree;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Queue;
+
+public class SimpleTreeClass<E extends Comparable<E>> implements SimpleTree<E> {
+    private Node<E> root;
+
+    public SimpleTreeClass(E root) {
+        this.root = new Node<>(root);
+    }
+
+    @Override
+    public boolean add(E parent, E child) {
+        boolean result = false;
+        Optional<Node<E>> optional = findBy(parent);
+        if (optional.isPresent()) {
+            Node<E> parentNode = optional.get();
+            List<Node<E>> leaves = parentNode.leaves();
+            Node<E> childNode = new Node<>(child);
+            if (leaves.stream().noneMatch(node -> node.eqValue(child))) {
+                parentNode.add(childNode);
+                result = true;
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public Optional<Node<E>> findBy(E value) {
+        Optional<Node<E>> rs1 = Optional.empty();
+        Queue<Node<E>> data = new LinkedList<>();
+        data.offer(this.root);
+        while (!data.isEmpty()) {
+            Node<E> el = data.poll();
+            if (el.eqValue(value)) {
+                rs1 = Optional.of(el);
+                break;
+            }
+            for (Node<E> child : el.leaves()) {
+                data.offer(child);
+            }
+        }
+        return rs1;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<>() {
+            Queue<Node<E>> queue = new LinkedList<>();
+
+            {
+                queue.offer(root);
+            }
+
+            @Override
+            public boolean hasNext() {
+                return !queue.isEmpty();
+            }
+
+            @Override
+            public E next() throws NoSuchElementException {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                var node = queue.poll();
+                if (!Objects.requireNonNull(node).leaves().isEmpty()) {
+                    for (var child : node.leaves()) {
+                        queue.offer(child);
+                    }
+                }
+                return node.getValue();
+            }
+        };
+    }
+}

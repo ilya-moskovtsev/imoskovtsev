e488026f1aa3fb5fecb6717e5e20d83a04039e42
ilya-moskovtsev <ilya.moskovtsev@gmail.com>
2019-07-11 21:36:22 +0300

    CRUD servlet, web app architecture [#155501]

--- middle/chapter_002_servlet_jsp/src/main/java/ru/job4j/servlets/crud/UpdateOperation.java
+++ middle/chapter_002_servlet_jsp/src/main/java/ru/job4j/servlets/crud/UpdateOperation.java
@@ -6,11 +6,15 @@ public class UpdateOperation implements Operation {
     @Override
     public void apply(Validate logicLayer, HttpServletRequest req) {
         String id = req.getParameter("id");
-        String newName = req.getParameter("name");
+        String name = req.getParameter("name");
+        String login = req.getParameter("login");
+        String email = req.getParameter("email");
 
         User user = logicLayer.findById(Integer.parseInt(id));
 
-        user.setName(newName);
+        user.setName(name);
+        user.setLogin(login);
+        user.setEmail(email);
 
         logicLayer.update(user);
     }
